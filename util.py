19cb3fd4604fa62068af8ee9ea34261ef5712f98
Suhil koul <suhilkoul123@gmail.com>
2020-06-22 20:09:38 +0530

    Update util.py

--- util.py
+++ util.py
@@ -27,13 +27,13 @@ def load_saved_artifacts():
     global  __data_columns
     global __locations
 
-    with open("./artifacts/columns.json", "r") as f:
+    with open("/artifacts/columns.json", "r") as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
 
     global __model
     if __model is None:
-        with open('./artifacts/pricePridictionModel.pickl', 'rb') as f:
+        with open('/artifacts/pricePridictionModel.pickl', 'rb') as f:
             __model = pickle.load(f)
     print("loading saved artifacts...done")
 
